8ccd3d64de3c888e401dc2206b47ad878f667571
russian_proger <sagitov.2806@mail.ru>
2025-04-27 16:54:23 +0300

    feat: title as an icon

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as THREE from "three";
 // import { color, fog, float, positionWorld, triNoise3D, positionView, normalWorld, uniform } from 'three/build/three.tsl';
 import { loadObjAsync, Renderer } from "expo-three";
-import { GLView } from "expo-gl";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 import '.';
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
@@ -16,7 +16,8 @@ import Cloud from "@/components/cloud";
 import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => {
-  const [gl, setGl] = React.useState<any>(null);
+  const [gl, setGl] = React.useState<ExpoWebGLRenderingContext|null>(null);
+  const [loading, setLoading] = React.useState(true);
 
   const pressed = useSharedValue<boolean>(false);
   const offset = useSharedValue<number>(0);
@@ -37,6 +38,8 @@ const App = () => {
     let request_id = 0;
     let timeout_id: any = 0;
 
+    if (!gl) return () => {};
+
     (async () => {
       const scene = new THREE.Scene();
       const camera = new THREE.OrthographicCamera();
@@ -44,12 +47,6 @@ const App = () => {
 
       camera.rotateX(-0.5);
       camera.position.y = 1.0;
-
-      gl.canvas = {
-        width: gl.drawingBufferWidth,
-        height: gl.drawingBufferHeight,
-      };
-  
       camera.position.z = 2;
   
       const renderer = new Renderer({ gl });
@@ -89,10 +86,11 @@ const App = () => {
         let finalValue = 0.75 + 1 / (1 + Math.exp(-offset.value / 100)) - 0.5;
         groundWrapper.rotation.y += (finalValue - groundWrapper.rotation.y) * 0.05;
         if (!groundShake) {
-          groundWrapper.position.y += -groundWrapper.position.y * 0.01;
+          groundWrapper.position.y += -groundWrapper.position.y * 0.03;
           if (Math.abs(groundWrapper.position.y) < 0.05) {
             groundShake = 1;
             groundShakeTimeStart = time;
+            setLoading(false);
           }
         } else {
           groundWrapper.position.y += -Math.sin((time - groundShakeTimeStart) * 0.0015) * 0.001;
@@ -124,54 +122,70 @@ const App = () => {
       {/* <LinearGradient colors={['#df729d', '#393790']}> */}
       <SafeAreaView style={styles.view}>
           {/* <Cloud/> */}
-          <View style={styles.titleContainer}>
-            <View style={styles.leftFigure}>
-              <View style={{...styles.figuresWrapper, ...styles.leftFiguresWrapper}}>
-                <View style={styles.figuresWrapperRel}>
-                  <View style={{...styles.block, ...styles.blockL1}} />
-                  <View style={{...styles.block, ...styles.blockL2}} />
-                  <View style={{...styles.block, ...styles.blockL3}} />
-                  <View style={{...styles.block, ...styles.blockL4}} />
+          <View style={styles.header}>
+            <View style={styles.titleContainer}>
+              <View style={styles.leftFigure}>
+                <View style={{...styles.figuresWrapper, ...styles.leftFiguresWrapper}}>
+                  <View style={styles.figuresWrapperRel}>
+                    <View style={{...styles.block, ...styles.blockL1}} />
+                    <View style={{...styles.block, ...styles.blockL2}} />
+                    <View style={{...styles.block, ...styles.blockL3}} />
+                    <View style={{...styles.block, ...styles.blockL4}} />
+                  </View>
+                </View>
+              </View>
+              <Text style={styles.titleText}>Мой район</Text>
+              <View style={styles.rightFigure}>
+                <View style={{...styles.figuresWrapper, ...styles.rightFiguresWrapper}}>
+                  <View style={styles.figuresWrapperRel}>
+                    <View style={{...styles.block, ...styles.blockR1}} />
+                    <View style={{...styles.block, ...styles.blockR2}} />
+                    <View style={{...styles.block, ...styles.blockR3}} />
+                    <View style={{...styles.block, ...styles.blockR4}} />
+                  </View>
                 </View>
               </View>
             </View>
-            <Text style={styles.titleText}>Мой район</Text>
-            <View style={styles.rightFigure}>
-              <View style={{...styles.figuresWrapper, ...styles.rightFiguresWrapper}}>
-                <View style={styles.figuresWrapperRel}>
-                  <View style={{...styles.block, ...styles.blockR1}} />
-                  <View style={{...styles.block, ...styles.blockR2}} />
-                  <View style={{...styles.block, ...styles.blockR3}} />
-                  <View style={{...styles.block, ...styles.blockR4}} />
-                </View>
-              </View>
-            </View>
+            { loading && <ActivityIndicator size="large" color="#ffffff55" /> }
           </View>
           <GestureDetector gesture={pan}>
             <GLView
               onContextCreate={gl => setGl(gl)}
-              style={{ width: '100%', height: 400, ...styles.canvas }}
+              style={{ ...styles.canvas }}
               />
-          </GestureDetector>
+          </GestureDetector> 
         </SafeAreaView>
       </LinearGradient>
     </GestureHandlerRootView>
   );
 };
 
+const blockWidth = 15;
+const blockPad = 3;
+
 const styles = StyleSheet.create({
   view: {
     height: '100%',
-    paddingTop: 200
+    paddingTop: 45
   },
   canvas: {
     position: 'absolute',
     bottom: 0,
+    width: '100%',
+    height: 400
   },
-  titleContainer: {
+  header: {
+    boxSizing: 'border-box',
     width: '100%',
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 40,
+    paddingRight: 20,
+  },
+  titleContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
     color: 'white',
     flexDirection: 'row',
   },
@@ -190,74 +204,58 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   leftFiguresWrapper: {
-    top: 5,
-    left: 1
+    top: -31,
+    left: 15
   },
   rightFiguresWrapper: {
-    top: 10,
-    left: 3,
+    top: 8,
+    left: 19,
   },
   rightFigure: {
     position: 'relative',
   },
   blockL1: {
-    left: 0,
-    top: -43
+    right: 0,
+    top: 0
   },
   blockL2: {
-    left: 0,
-    top: -20,
+    right: 0,
+    top: blockPad + blockWidth,
   },
   blockL3: {
-    left: -23,
-    top: -20,
+    right: blockPad + blockWidth,
+    top: blockPad + blockWidth,
   },
   blockL4: {
-    left: -23,
-    top: 3,
+    right: blockPad + blockWidth,
+    top: (blockPad + blockWidth) * 2,
   },
-  // blockR1: {
-  //   left: -23,
-  //   top: -20,
-  // },
-  // blockR2: {
-  //   left: 0,
-  //   top: -20,
-  // },
-  // blockR3: {
-  //   left: 0,
-  //   top: 3,
-  // },
-  // blockR4: {
-  //   left: 0,
-  //   top: 26,
-  // },
   blockR1: {
-    left: 0,
-    top: 3,
+    right: 0,
+    top: 0
   },
   blockR2: {
-    left: 0,
-    top: 26,
+    right: 0,
+    top: blockPad + blockWidth,
   },
   blockR3: {
-    right: 3,
-    top: 26,
+    right: blockPad + blockWidth,
+    top: blockPad + blockWidth,
   },
   blockR4: {
-    right: 26,
-    top: 26,
+    right: (blockPad + blockWidth) * 2,
+    top: blockPad + blockWidth,
   },
   block: {
     position: 'absolute',
-    width: 20,
-    height: 20,
+    width: blockWidth,
+    height: blockWidth,
     backgroundColor: '#fffa',
     borderRadius: 3,
   },
   titleText: {
     color: 'white',
-    fontSize: 30,
+    fontSize: 20,
   }
 })
 
