99f81231ce1fede92241c24bc460c812de8919d8
russian_proger <sagitov.2806@mail.ru>
2025-05-01 13:44:18 +0300

    change background, add icon library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false, statusBarStyle: 'light', statusBarBackgroundColor: '#323f94' }} />
-      <Stack.Screen name="auth" options={{ headerShown: false, statusBarStyle: 'light', statusBarBackgroundColor: '#323f94' }} />
+      <Stack.Screen name="index" options={{ headerShown: false, statusBarStyle: 'light', statusBarBackgroundColor: '#393790' }} />
+      <Stack.Screen name="auth" options={{ headerShown: false, statusBarStyle: 'light', statusBarBackgroundColor: '#393790' }} />
     </Stack>
   );
 }
