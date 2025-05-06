8d2a34f6a9546c4e9a540ea764a388c1f1475298
russian_proger <sagitov.2806@mail.ru>
2025-05-07 00:03:00 +0300

    feat: add theme check

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,14 @@
 import { Redirect } from "expo-router";
-
+import { useMMKVString } from "react-native-mmkv";
+import { LIGHT_THEME, THEMES } from "@/core/theme";
 
 export default function App() {
+  const [theme, setTheme] = useMMKVString('theme');
+
+  if (!THEMES.includes(theme!)) {
+    setTheme(LIGHT_THEME);
+    return null;
+  }
+
   return <Redirect href={'/auth'} />
 }
